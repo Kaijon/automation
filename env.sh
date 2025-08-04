9d9003e50347abf7e0ff4f8c34b11bfb6fb0190f
KaiChung Hou <kaijon0125@gmail.com>
2025-08-04 13:59:42 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,10 +1,58 @@
 #!/bin/sh
-export CA42_USERNAME='root'
-export CA42_PASSWORD='getac123'
-export RTSP_USERNAME='admin'
-export RTSP_PASSWORD='4200'
-export #TC_TOKEN="eyJ0eXAiOiAiVENWMiJ9.Ui1NNlR3Zk4zVTVsbm9MMENqbWplWHBjZlhr.NzMyZWUzM2MtOTAyOC00MDA1LTlkMDQtYjRlYmQ4YTkyZGQx"
+
+# Check if the first argument is empty
+if [ -z "$1" ]; then
+    echo "Error: Missing argument."
+    echo "Usage: $0 <CA42 | CA42-A | CA22-G2>"
+    exit 1
+fi
+
 export TC_TOKEN='eyJ0eXAiOiAiVENWMiJ9.dW0tb3E0bnRrZjNpWlhZZkIwVW5Hb2RlYy1V.NmFmNTljN2QtMGRmZC00YzA4LTkzOWMtOWJmMjZkZGE2YmYy'
+
+# Get the argument
+ARG="$1"
+
+# Use case statement for better scalability
+case "$ARG" in
+    "CA42")
+        echo "Running stage for CA42..."
+		export DEVICE_IP='192.168.5.51'
+		export USERNAME='root'
+		export PASSWORD='getac123'
+		export RTSP_USERNAME='admin'
+		export RTSP_PASSWORD='4200'
+		export PROJECT_ROOT='home/jenkins/ca42-aumation'
+        ;;
+    "CA42-A")
+        echo "Running stage for CA42-A..."
+		export DEVICE_IP='192.168.5.52'
+		export USERNAME='root'
+		export PASSWORD='getac123'
+		export RTSP_USERNAME='admin'
+		export RTSP_PASSWORD='4200'
+		export PROJECT_ROOT='home/jenkins/ca42a-aumation'
+        ;;
+	"CA22-G2")
+	    echo "Running stage for CA22-G2..."
+		export DEVICE_IP='192.168.5.X'
+		export USERNAME='root'
+		export PASSWORD='getac123'
+		export RTSP_USERNAME='admin'
+		export RTSP_PASSWORD='2100'
+		export PROJECT_ROOT='home/jenkins/ca22-g2-aumation'
+    *)
+        echo "Error: Invalid argument."
+        echo "Please specify a correct argument: CA42 or CA42-A"
+        exit 1
+        ;;
+esac
+
 # Verify they are set:
-echo $CA42_USERNAME
-echo $TC_TOKEN
+echo "DEVICE_IP=$DEVICE_IP"
+echo "USERNAME=$USERNAME"
+echo "PASSWORD=$PASSWORD"
+echo "RTSP_USERNAME=$RTSP_USERNAME"
+echo "RTSP_PASSWORD=$RTSP_PASSWORD"
+echo "TC_TOKEN=$TC_TOKEN"
+
+exit 0
